b21e44f63ed2c9a2baeeb6f5af0446f89a8cd96b
rohan <mbakibillahrohan@gmail.com>
2023-10-14 08:52:49 +0600

    add unit name with auth data

--- services/authentication_services/login_services.js
+++ services/authentication_services/login_services.js
@@ -11,23 +11,24 @@ const loginServices = async (payload) => {
     const userInfo = await getUserInfo(payload);
     const isUserValid = checkValidity(userInfo, payload);
     if (isUserValid) {
-        const { TypeName, IsWashing, IsSewing, FullName, UnitId, UserId, IsFinishing } = userInfo;
+        const { TypeName, IsWashing, IsSewing, FullName, UnitId, UserId, IsFinishing, UnitName } = userInfo;
         const userType = getUserType(TypeName);
         userInfo.UserType = userType;
         token = generateJwtToken(userInfo);
         userData = {
-            IsWashing, IsSewing, IsFinishing, FullName, UnitId, UserId, ChallanApprovalUserType: userType, token
+            IsWashing, IsSewing, UnitName, IsFinishing, FullName, UnitId, UserId, ChallanApprovalUserType: userType, token
         }
     }
     return userData;
 }
 
 const getUserInfo = async (payload) => {
-    const query = `select ui.UserId, ui.EmpId, ui.UserName, cpt.ChallanPermissionType TypeName, ui.CompanyId,
-                    ui.branch_code UnitId, ui.FullName, ui.LineId, ui.UsrPass, ui.IsSewing, ui.IsWashing, ui.IsFinishing from UserInfo ui
-                    left join ChallanApprovalPermission cap on cap.UserId = ui.UserId
-                    left join ChallanPermissionType cpt on cpt.CPTId = cap.CPTId
-                    where UserName = @UserName`;
+    const query = `select ui.UserId, ui.EmpId, ui.UserName, u.UnitName, cpt.ChallanPermissionType TypeName, ui.CompanyId,
+    ui.branch_code UnitId, ui.FullName, ui.LineId, ui.UsrPass, ui.IsSewing, ui.IsWashing, ui.IsFinishing from UserInfo ui
+    left join ChallanApprovalPermission cap on cap.UserId = ui.UserId
+    left join ChallanPermissionType cpt on cpt.CPTId = cap.CPTId
+	left join Unit u on u.UnitId = ui.branch_code
+    where UserName = @UserName`;
     const parameters = [
         {
             name: "UserName",
