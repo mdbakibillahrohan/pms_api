97f4e3d71f0e185ac36df3dc118c719a22a9639e
rohan <mbakibillahrohan@gmail.com>
2023-10-31 19:12:56 +0600

    fixed the line-production-target-dhu problem

--- services/finishing_dashboard_services/line_wise_target_production_dhu_services.js
+++ services/finishing_dashboard_services/line_wise_target_production_dhu_services.js
@@ -10,13 +10,13 @@ const lineWiseTargetProductionDHUServices = async (payload)=>{
 const getLineWiseTargetProductionDHU = async (payload)=>{
     const {unitId} = payload;
     const date = getDate(payload);
-    const query = `select ln.LineName, count(hfpc.ChildBarcode) Production, sum((swft.HourlyTarget*8)) Target,
+    const query = `select ln.LineName, count(hfpc.ChildBarcode) Production, (swft.HourlyTarget*8) Target,
     ((select count(ChildBarcode) Defect from HourlyFinishingDefectCount with(nolock) where ProductionDate = ${date} and SewingLineId = ln.LineId)/count(hfpc.ChildBarcode)*100) DHU
     from HourlyFinishingProductionCount hfpc with(nolock)
     left join StyleWiseFinishingTarget swft with(nolock) on swft.StyleId = hfpc.StyleId
     left join LineNew ln with(nolock) on ln.LineId = hfpc.SewingLineId 
     where hfpc.ProductionDate = ${date} and hfpc.UnitId = ${unitId}
-    group by LineName, ln.LineId`;
+    group by LineName, ln.LineId, swft.HourlyTarget`;
     const data = await getData(dbConfig, query);
     return data;
 }
