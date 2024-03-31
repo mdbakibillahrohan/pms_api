ec57093b1eb7a217bb01325fed902de1a5c6e287
AdamBinShowkot <adambinshowkot@gmail.com>
2024-03-31 15:40:09 +0600

    Change Descriton Column On New Tender Publish.

--- services/tender/get_publish_tender_lists/get_publish_tender_lists_services.js
+++ services/tender/get_publish_tender_lists/get_publish_tender_lists_services.js
@@ -25,7 +25,7 @@ const getTenderLists = async (payload)=>{
     }=payload;
     const query = `select 
     ROW_NUMBER() OVER (ORDER BY (SELECT 1)) AS [key],
-       A.TenderBidId,B.TenderNo,B.TenderNo+' - '+B.TenderTitle as TenderNoTitle,B.MinimumBidAmount,A.OpenDate,
+       A.TenderBidId,A.Description,B.TenderNo,B.TenderNo+' - '+B.TenderTitle as TenderNoTitle,B.MinimumBidAmount,A.OpenDate,
 	   DATEADD(MINUTE,ISNULL((SELECT SUM(K.Minutes) FROM TimerLogs K WHERE K.TenderBidId=A.TenderBidId),0),A.CloseDate) CloseDate,
 	   (
         case when DATEDIFF(second,A.OpenDate,GETDATE())>1 and DATEDIFF(second,DATEADD(MINUTE,ISNULL((SELECT SUM(K.Minutes) FROM TimerLogs K WHERE K.TenderBidId=A.TenderBidId),0),A.CloseDate),GETDATE())<1 and A.IsSale=0 then 'On Going'
@@ -39,7 +39,7 @@ const getTenderLists = async (payload)=>{
        inner join Tender B on A.TenderId=B.TenderId
     where 
     A.IsDeleted=0 
-    group by A.TenderBidId,B.TenderNo,B.TenderTitle,B.MinimumBidAmount,A.OpenDate,A.CloseDate,A.IsSale
+    group by A.TenderBidId,A.Description,B.TenderNo,B.TenderTitle,B.MinimumBidAmount,A.OpenDate,A.CloseDate,A.IsSale
     order by A.TenderBidId desc OFFSET ${Skip} ROWS 
     FETCH NEXT ${Take} ROWS ONLY`;
     const data = await getData(dbConfig3, query);
