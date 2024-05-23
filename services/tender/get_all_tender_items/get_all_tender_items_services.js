3843a5d98f9c035ce8c28f929357cf4cd39a0558
AdamBinShowkot <adambinshowkot@gmail.com>
2024-05-23 11:43:57 +0600

    Price History bug solved.

--- services/tender/get_all_tender_items/get_all_tender_items_services.js
+++ services/tender/get_all_tender_items/get_all_tender_items_services.js
@@ -15,14 +15,15 @@ const getTenderItemsListsServices = async (payload)=>{
 }
 
 const getTenderLists = async (payload)=>{
-    const query = ` select 
-    A.ItemId,A.ItemName as [value] 
-    from TenderItems A 
+    const query = `select 
+    A.ItemId,A.ItemName+' -- '+ISNULL(E.GradeName, 'N/A') as [value] 
+    from TenderItems A
+	left join TenderGrade E on A.TenderGradeId=E.TenderGradeId
     inner join BiddingDetails B on A.ItemId=B.ItemId  
     inner join Bidding C on B.BiddingId=C.BiddingId
     inner join TenderBidLists D on C.TenderBidId=D.TenderBidId
     where DATEDIFF(second,D.CloseDate,GETDATE())>1
-    group by A.ItemId,A.ItemName
+    group by A.ItemId,A.ItemName,E.GradeName
     order by A.ItemId desc`;
     const data = await getData(dbConfig3, query);
     return data; 
--- services/tender/get_item_price_date/get_item_price_date_services.js
+++ services/tender/get_item_price_date/get_item_price_date_services.js
@@ -16,11 +16,21 @@ const getTenderItemsListsServices = async (payload)=>{
 
 const getTenderLists = async (payload)=>{
     const {ItemName,ItemId}=payload;
-    const query = `  select B.ItemId,B.ItemName,isnull(Max(A.BidPrice),0) as BidPrice,A.CreatedAt from BiddingDetails A
+    const query = `select B.ItemId,B.ItemName,isnull(Max(A.BidPrice),0) as BidPrice,A.CreatedAt from BiddingDetails A
     inner join TenderItems B on A.ItemId=B.ItemId
     where B.ItemName like '%${ItemName}%' and B.ItemId=${ItemId}
     and 
-    A.BidPrice=ISNULL((select max(AA.BidPrice) from BiddingDetails AA inner join TenderItems BB on AA.ItemId=BB.ItemId   where BB.ItemName like '%${ItemName}%' and BB.ItemId=${ItemId}),0)
+    A.BidPrice=ISNULL((
+		SELECT Max(BD.BidPrice)
+		FROM BiddingDetails BD
+		JOIN (
+			SELECT TenderUserId, MAX(CreatedAt) AS MaxCreatedAt
+			FROM BiddingDetails
+			WHERE ItemId = ${ItemId}
+			GROUP BY TenderUserId
+		) AS MaxDates ON BD.TenderUserId = MaxDates.TenderUserId AND BD.CreatedAt = MaxDates.MaxCreatedAt
+		WHERE BD.ItemId = ${ItemId}
+	),0)
     group by B.ItemId,B.ItemName,A.CreatedAt`;
     const data = await getData(dbConfig3, query);
     return data; 
